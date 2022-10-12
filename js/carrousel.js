ea0811d2baed13f7d0827028084e83f7fb6876e1
nicolasbava <nicobava96@gmail.com>
2022-10-12 19:56:46 -0300

    feat: nico agrego estilos en la parte de servicios para hacer que el swiper tambien cambie el texto, falta terminar

--- js/carrousel.js
+++ js/carrousel.js
@@ -7,12 +7,13 @@ var swiper = new Swiper(".mySwiper", {
         coverflowEffect: {
           rotate: 0,
           stretch: 80,
-          depth: 100,
+          depth: 200,
           modifier: 2,
           slideShadows: true
         },
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev"
-        }
+        },
+        noSwiping: true
       });
