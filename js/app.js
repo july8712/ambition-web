0ff73dbb191e9f85c20799bbb962c7da4ca4c24e
Nicolas <nicobava96@gmail.com>
2022-10-14 12:42:07 -0300

    feat: added contact section, added send email from contact form feature

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+console.log('PASE POR ACA');
+
+window.addEventListener('load',
+{}
+)
 
 window.addEventListener("scroll", function() {
    let nav = document.querySelector("nav");
@@ -5,8 +10,6 @@ window.addEventListener("scroll", function() {
 })
 
 let cardProyecto = document.querySelector(".cardProyecto");
-
-
 
 const proyectos = document.querySelectorAll(".cardProyecto");
 proyectos.forEach((proyecto) => {
@@ -28,4 +31,77 @@ function cerrarProyecto(id){
 }
 
 
+// ======= SWIPER TEXT PART =======
 
+const swiperMod = document.querySelector('.swiperMod')
+const style = getComputedStyle(swiperMod);
+
+
+
+let index 
+let positionY 
+let positionX  
+
+function getIndex(){
+   index = style.zIndex
+   return index
+}
+
+
+
+
+setInterval(() => {
+   const matrix = new WebKitCSSMatrix(style.transform);
+   // transform: matrix3d(m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44)
+
+   function getPositionY(){
+      positionY = matrix.m41
+      return positionY
+   }
+   
+   function getPositionX(){
+      positionX = matrix.m43
+      return positionX
+   }
+
+   let swiperText1 = document.querySelector('#slide-1')
+   let swiperText2 = document.querySelector('#slide-2')
+   let swiperText3 = document.querySelector('#slide-3')
+ 
+ 
+   getIndex();
+   getPositionY();
+   getPositionX();
+
+  console.log( getIndex());
+  console.log(getPositionY());
+  console.log(getPositionX());
+
+   console.log(swiperText1)
+
+  
+   console.log('INDEX', index)
+   if( index === -5 ){
+
+      swiperText1.classList.add('slide-active')
+
+
+   }
+
+
+
+}, 500)
+
+
+
+/* 
+
+index 1 : 1 0 0 showing
+index 1: -5 -1200 480 showing 
+
+index 2 compu : -1 -400 160
+
+index 3 colores: -3 -800 320
+index 3 colores: -1 -400 -160
+
+*/
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
@@ -17,3 +17,28 @@ var swiper = new Swiper(".mySwiper", {
         },
         noSwiping: true
       });
+      
+const swiperText = new Swiper(".mySwiperText", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 200,
+    modifier: 2,
+    slideShadows: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  noSwiping: false
+});
+
+swiper[0].controller.control = swiperText;
+swiperText[0].controller.control = swiper; 
+
+
